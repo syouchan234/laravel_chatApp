f9b53eed6ce944f4a86262e3e7cadd1d6f2df043
サブPC <syou5963@outlook.jp>
2024-04-04 13:21:21 +0900

    テストデータ生成ファイルの修正

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,32 +5,45 @@ namespace Database\Seeders;
 use App\Models\ToDo;
 use App\Models\ToDoDetail;
 use App\Models\Account;
+use App\Models\Comments;
 use App\Models\Post;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        // ToDo情報のサンプルデータの作成
-        $todo = ToDo::factory()->create();
-        ToDoDetail::factory(5)->create([
-            'to_do_id' => $todo->id
-        ]);
-
-        // // サンプルアカウントデータの作成
-        // Account::create([
-        //     'name' => 'testuser',
-        //     'account_name' => 'testerMAN',
-        //     'email' => 'test@examle.com',
-        //     'password' => Hash::make('password'),
+        // // ToDo情報のサンプルデータの作成
+        // $todo = ToDo::factory()->create();
+        // ToDoDetail::factory(5)->create([
+        //     'to_do_id' => $todo->id
         // ]);
+        
+        // サンプルアカウントデータの作成
+        // Account::factory()->create();
 
         // サンプルアカウントデータの作成
-        Account::factory(5)->create();
+        Account::create([
+            'name' => 'testuser',
+            'account_name' => 'testerMAN',
+            'email' => 'test@examle.com',
+            'password' => Hash::make('password'),
+        ]);
 
-        // データを挿入する処理
-        Post::factory()->count(10)->create();
+        // // サンプル投稿データの作成
+        // $posts = Post::factory()->count(5)->create();
+
+        // サンプルコメントデータの作成
+        // foreach ($posts as $post) {
+        //     Comments::factory(5)->create([
+        //         'post_id' => $post->id
+        //     ]);
+        // }
+        
+        // Account::factory()->create();//アカウントデータのみが作成される
+        // Post::factory()->create();//postデータとaccountデータが同時に作成れる。
+        // Comments::factory()->create();//commentデータとpostデータとaccountデータが同時に作成れる。
     }
 }
