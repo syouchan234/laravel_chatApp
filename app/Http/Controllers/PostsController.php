ef00f69e8883dda0c1c003682cc3d9069d74ed6a
サブPC <syou5963@outlook.jp>
2024-04-16 14:55:30 +0900

    ひとまずcorsorのせいで消えたアカウント名の取得情報ファイルの修理

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -9,20 +9,40 @@ use Illuminate\Http\Request;
 class PostsController extends Controller
 {
     //投稿一覧を取得する処理
-    public function index(){
-        //コメントと一緒に投稿を取得する最新３０件
-        $postList = Post::with('comments')->latest()->take(30)->get();
-        //取得情報を返却する
+    public function index()
+    {
+        // 最新の投稿一覧を取得し、関連するアカウント情報も取得する
+        $noFormattedPosts = Post::with(['account', 'comments'])->latest()->take(30)->get();
+
+        // 必要な情報だけを返すように整形
+        $postList = $noFormattedPosts->map(function ($post) {
+            return [
+                'id' => $post->id,
+                'account_name' => $post->account->name, // アカウントの名前を取得
+                'title' => $post->title,
+                'content' => $post->content,
+                'created_at' => $post->created_at,
+                'updated_at' => $post->updated_at,
+                'comments' => $post->comments // ここでコメントも取得する必要があれば加工する
+            ];
+        });
+
+        // 取得した投稿一覧を返す
         return $postList;
     }
 
     //投稿する処理
-    public function store(StoreRequest $request){
+    public function store(StoreRequest $request)
+    {
         //新規のpostモデルを作成する
         $post = new Post();
         //各情報をモデルに設定する
-        $post->content = $request->get('content','account_id');
+        $post->content = $request->input('content');
+        $post->account_id = $request->input('account_id');
         //DBに登録する
         $post->save();
+
+        //保存が完了したら適切なレスポンスを返す
+        return response()->json(['message' => '投稿が完了しました。'], 201);
     }
 }
