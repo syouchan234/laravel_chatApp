d5b1cd50914155a6bf04631c21b44e500acc7087
サブPC <syou5963@outlook.jp>
2024-04-19 11:32:13 +0900

    プロフィールの取得APIの作成

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -10,9 +10,18 @@ class ProfilesController extends Controller
     public function getProfile(Request $request)
     {
         $user = $request->user(); // ログイン中のユーザー情報を取得
-
+        $profile = $user->profile ?? null;
+        
         return response()->json([
-            'accounts' => $user
+            'name' => $user->name,
+            'account_name' => $user->account_name,
+            'email' => $user->email,
+            'created_at' => $user->created_at,
+            'updated_at' => $profile ? $profile->updated_at : $user->created_at,
+            'gender' => $profile ? $profile->gender : null,
+            'place' => $profile ? $profile->place : null,
+            'birthday' => $profile ? $profile->birthday : null,
+            'introduction' => $profile ? $profile->introduction : null,
         ]);
     }
 
@@ -20,9 +29,16 @@ class ProfilesController extends Controller
     {
         $account = Account::findOrFail($accountId); // 外部アカウントからのアカウントIDでアカウント情報を取得
         $posts = Post::where('account_id', $accountId)->get(); // 外部アカウントからのユーザーが投稿した全ての投稿データを取得
+        $profile = $account->profile ?? null;
 
         return response()->json([
-            'account' => $account,
+            'account_name' => $account->account_name,
+            'created_at' => $account->created_at,
+            'updated_at' => $profile? $profile->updated_at : $account->created_at,
+            'gender' => $profile ? $profile->gender : null,
+            'place' => $profile ? $profile->place : null,
+            'birthday' => $profile ? $profile->birthday : null,
+            'introduction' => $profile ? $profile->introduction : null,
             'posts' => $posts
         ]);
     }
